0ef7097c3f468c255ede5c923d3b2195bf030560
Xsudo <dkiedrowicz@o2.pl>
2020-02-02 12:50:22 +0100

    Initial commit

---
+++ src/main/java/com/example/ReservationApi/ReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReservationApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReservationApi/ReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReservationApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
